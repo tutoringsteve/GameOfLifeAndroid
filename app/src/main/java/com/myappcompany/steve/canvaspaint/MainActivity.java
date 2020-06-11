1c2b27266a0c2feeb5216bf292eb89f6d28a0fc5
tutoringsteve <tutoringsteve@gmail.com>
2020-06-11 18:43:44 -0400

    Added an autoplay button.

--- app/src/main/java/com/myappcompany/steve/canvaspaint/MainActivity.java
+++ app/src/main/java/com/myappcompany/steve/canvaspaint/MainActivity.java
@@ -1,11 +1,14 @@
 package com.myappcompany.steve.canvaspaint;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     private int numColumns = 17;
     private int numRows = 17;
     private int maxTurns = 32;
+    private boolean isAutoPlaying = false;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         PixelGridView pixelGrid = findViewById(R.id.pixelGridView2);
         pixelGrid.setNumColumns(numColumns);
         pixelGrid.setNumRows(numRows);
+
+        handler = new Handler();
 
     }
 
@@ -35,6 +42,27 @@ public class MainActivity extends AppCompatActivity {
         boardState = board.getBooleanGameBoard();
         pixelBoard.setCellChecked(boardState);
 
+    }
+
+    public void autoClick(final View view) {
+
+        isAutoPlaying = !isAutoPlaying;
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                onClick(view);
+                handler.postDelayed(this, 1000);
+            }
+        };
+
+        if(isAutoPlaying) {
+            Toast.makeText(MainActivity.this,"isAutoPlaying is true", Toast.LENGTH_SHORT).show();
+            handler.post(runnable);
+        } else {
+            Toast.makeText(MainActivity.this,"isAutoPlaying is false", Toast.LENGTH_SHORT).show();
+            handler.removeCallbacksAndMessages(null);
+        }
     }
 
 
