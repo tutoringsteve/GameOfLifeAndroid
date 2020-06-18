0753a602b3da0fbc867b58c8a83b698a466dc180
tutoringsteve <tutoringsteve@gmail.com>
2020-06-17 23:28:32 -0400

    Grid can be drawn partially offscreen and added logic to only draw the grid rectangles that are partially on screen for efficieny

--- app/src/main/java/com/myappcompany/steve/canvaspaint/PixelGridView.java
+++ app/src/main/java/com/myappcompany/steve/canvaspaint/PixelGridView.java
@@ -14,10 +14,13 @@ import android.view.View;
 
 public class PixelGridView extends View {
     private int numColumns, numRows;
-    private int offsetX = 0, offsetY = 0;
-    private int cellWidth, cellHeight;
+    private int offsetX = -50, offsetY = 100;
+    private float zoomX = 1.0f, zoomY = 1.0f;
+    private int cellWidth = 20, cellHeight = 20;
     private Paint blackPaint = new Paint();
     private Paint bluePaint = new Paint();
+    private Paint darkGreyPaint = new Paint();
+    private Paint whitePaint = new Paint();
     private boolean[][] cellChecked;
     private boolean isEditing = true;
 
@@ -29,7 +32,11 @@ public class PixelGridView extends View {
         super(context, attrs);
         blackPaint.setStyle(Paint.Style.FILL_AND_STROKE);
         bluePaint.setStyle(Paint.Style.FILL_AND_STROKE);
-        bluePaint.setColor(Color.parseColor("#ff0000ff"));
+        bluePaint.setColor(Color.BLUE);
+        darkGreyPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        darkGreyPaint.setColor(Color.DKGRAY);
+        whitePaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        whitePaint.setColor(Color.WHITE);
     }
 
     public boolean[][] getCellChecked() {
@@ -73,41 +80,68 @@ public class PixelGridView extends View {
             return;
         }
 
-        cellWidth = getWidth() / numColumns;
-        cellHeight = getHeight() / numRows;
-
         cellChecked = new boolean[numColumns][numRows];
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawColor(Color.WHITE);
+        //draw the background dark grey so that off the grid appears dark grey
+        canvas.drawColor(Color.DKGRAY);
 
         if (numColumns == 0 || numRows == 0) {
             return;
         }
 
-        int width = getWidth();
-        int height = getHeight();
+        int viewWidth = getWidth();
+        int viewHeight = getHeight();
 
-        for (int i = 0; i < numColumns; i++) {
-            for (int j = 0; j < numRows; j++) {
-                if (cellChecked[i][j]) {
+        //determines the grids left side on PixelGridView, bounding between 0 and screen width of View
+        int minGridX = (offsetX < 0 ? 0 : Math.min(offsetX, viewWidth));
+        //determines the grids right side on PixelGridView, bounding between 0 and screen width of View
+        int maxGridX = (offsetX > viewWidth ? viewWidth : Math.min(viewWidth, offsetX + cellWidth*numColumns));
+        //determines the grids top side on PixelGridView, bounding between 0 and screen width of View
+        int minGridY = (offsetY < 0 ? 0 : Math.min(offsetY, viewHeight));
+        //determines the grids bottom side on PixelGridView, bounding between 0 and screen height of View
+        int maxGridY = (offsetY > viewHeight ? viewHeight : Math.min(viewHeight, offsetY + cellHeight*numRows));
 
-                    canvas.drawRect(i * cellWidth, j * cellHeight,
-                            (i + 1) * cellWidth, (j + 1) * cellHeight,
-                            bluePaint);
+        //guarantee that grid is on screen before drawing
+        if( minGridX < maxGridX && minGridY < maxGridY) {
+
+            //draw the part of the grid that overlaps with the View
+            canvas.drawRect(minGridX, minGridY, maxGridX, maxGridY, whitePaint);
+
+            //todo: make this more efficient by checking whether the rectangle is fully on screen
+            for (int i = 0; i < numColumns; i++) {
+                for (int j = 0; j < numRows; j++) {
+                    if (cellChecked[i][j]) {
+                        //Makes sure that at least part of the square is on screen before drawing it
+                        if((worldXToScreenX(i * cellWidth) > 0 && worldXToScreenX(i * cellWidth) < viewWidth)
+                                || (worldXToScreenX((i + 1) * cellWidth) > 0 && worldXToScreenX((i + 1) * cellWidth) < viewWidth)
+                                && (worldYToScreenY(j * cellHeight) > 0 && worldYToScreenY(j * cellHeight) < viewHeight)
+                                || (worldYToScreenY((j + 1) * cellHeight) > 0 && worldYToScreenY((j + 1) * cellHeight) < viewHeight))
+                        {
+                            canvas.drawRect(worldXToScreenX(i * cellWidth), worldYToScreenY(j * cellHeight),
+                                    worldXToScreenX((i + 1) * cellWidth), worldYToScreenY((j + 1) * cellHeight),
+                                    bluePaint);
+                        }
+                    }
+                }
+            }
+
+            for (int i = 1; i < numColumns; i++) {
+                if(minGridX <= i * cellWidth && i * cellWidth <= maxGridX) {
+                    canvas.drawLine(worldXToScreenX(i * cellWidth), minGridY, worldXToScreenX(i * cellWidth), maxGridY, blackPaint);
+                }
+            }
+
+            for (int i = 1; i < numRows; i++) {
+                if(minGridY <= i * cellHeight && i * cellHeight <= maxGridY) {
+                    canvas.drawLine(minGridX, worldYToScreenY(i * cellHeight), maxGridX, worldYToScreenY(i * cellHeight), blackPaint);
                 }
             }
         }
 
-        for (int i = 1; i < numColumns; i++) {
-            canvas.drawLine(i * cellWidth, 0, i * cellWidth, height, blackPaint);
-        }
 
-        for (int i = 1; i < numRows; i++) {
-            canvas.drawLine(0, i * cellHeight, width, i * cellHeight, blackPaint);
-        }
     }
 
     @Override
@@ -150,5 +184,37 @@ public class PixelGridView extends View {
 
     public void setOffsetY(int offsetY) {
         this.offsetY = offsetY;
+    }
+
+    public float getZoomX() {
+        return zoomX;
+    }
+
+    public void setZoomX(float zoomX) {
+        this.zoomX = zoomX;
+    }
+
+    public float getZoomY() {
+        return zoomY;
+    }
+
+    public void setZoomY(float zoomY) {
+        this.zoomY = zoomY;
+    }
+
+    public void setCellWidth(int cellWidth) {
+        this.cellWidth = cellWidth;
+    }
+
+    public void setCellHeight(int cellHeight) {
+        this.cellHeight = cellHeight;
+    }
+
+    public float worldXToScreenX(float worldX) {
+        return worldX + offsetX;
+    }
+
+    public float worldYToScreenY(float worldY) {
+        return worldY + offsetY;
     }
 }
