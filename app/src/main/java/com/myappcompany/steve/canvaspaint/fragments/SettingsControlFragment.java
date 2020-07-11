d9b78c27ac4ee33bb24be6fb1ae20713ad16a1b4
tutoringsteve <tutoringsteve@gmail.com>
2020-07-11 16:23:14 -0400

    Setup edit text and check box listeners for the grid control settings tab.

--- app/src/main/java/com/myappcompany/steve/canvaspaint/fragments/SettingsControlFragment.java
+++ app/src/main/java/com/myappcompany/steve/canvaspaint/fragments/SettingsControlFragment.java
@@ -30,12 +30,12 @@ public class SettingsControlFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
         mSettingsControlView = inflater.inflate(R.layout.fragment_settings_control, container, false);
 
         setupAutoPlaySpeedSpinner();
         setupRandomFillProbabilitySpinner();
 
-        // Inflate the layout for this fragment
         return mSettingsControlView;
     }
 
--- app/src/main/java/com/myappcompany/steve/canvaspaint/fragments/SettingsGridFragment.java
+++ app/src/main/java/com/myappcompany/steve/canvaspaint/fragments/SettingsGridFragment.java
@@ -4,9 +4,15 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.myappcompany.steve.canvaspaint.R;
 
@@ -14,6 +20,8 @@ import com.myappcompany.steve.canvaspaint.R;
  * A simple {@link Fragment} subclass.
  */
 public class SettingsGridFragment extends Fragment {
+    private static final String TAG = "SettingsGridFragment";
+    private View view;
 
     public SettingsGridFragment() {
         // Required empty public constructor
@@ -23,6 +31,65 @@ public class SettingsGridFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings_grid, container, false);
+        view = inflater.inflate(R.layout.fragment_settings_grid, container, false);
+
+        setupBoardWidthEditText();
+        setupBoardHeightEditText();
+        setupHorizontalWrappingCheckBox();
+        setupVerticalWrappingCheckBox();
+        return view;
+    }
+
+    private void setupBoardWidthEditText() {
+        EditText boardWidthEditText = view.findViewById(R.id.boardWidthEditText);
+        boardWidthEditText.setOnKeyListener(new EditText.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
+                    String text = ((EditText) v).getText().toString();
+                    Log.d(TAG, "boardWidthEditText changed set to " + text);
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+    }
+
+    private void setupBoardHeightEditText() {
+        EditText boardHeightEditText = view.findViewById(R.id.boardHeightEditText);
+        boardHeightEditText.setOnKeyListener(new EditText.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
+                    String text = ((EditText) v).getText().toString();
+                    Log.d(TAG, "boardHeightEditText changed set to " + text);
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+
+    }
+
+    private void setupHorizontalWrappingCheckBox() {
+        CheckBox horizontalWrappingCheckBox = view.findViewById(R.id.horizontalWrappingCheckBox);
+        horizontalWrappingCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.d(TAG, "horizontal wrapping check box set to " + (isChecked? "Checked" : "Not Checked"));
+            }
+        });
+    }
+
+    private void setupVerticalWrappingCheckBox() {
+        CheckBox verticalWrappingCheckBox = view.findViewById(R.id.verticalWrappingCheckBox);
+        verticalWrappingCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.d(TAG, "vertical wrapping check box set to " + (isChecked? "Checked" : "Not Checked"));
+            }
+        });
     }
 }
